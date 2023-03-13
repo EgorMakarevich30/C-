4142c6eba67278652e90070e18a48ce2217c71a3
EgorMakarevich30 <7307722@gmail.com>
2023-03-13 16:43:56 +0300

    Homework 04

---
+++ C Seminar 04/Homework 04/Program.cs
@@ -0,0 +1,115 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// START // ———————————————————————————————————————————————————————————————————————— 1 variant
+
+// double NumToPower(int a, int b)
+// {
+//     double power = Math.Pow(a, b);
+//     return power;
+// }
+
+// Console.Write("Input number A: ");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number B: ");
+// int numB = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(NumToPower(numA, numB));
+
+
+// START // ———————————————————————————————————————————————————————————————————————— 2 variant
+
+// double InputNum(string num)
+// {
+//     Console.Write($"Input number {num}: ");
+//     return Convert.ToDouble(Console.ReadLine());
+// }
+
+// double numberA = InputNum("A");
+// double numberB = InputNum("B");
+// double power = Math.Pow(numberA, numberB);
+// Console.WriteLine($"{numberA} to the power of {numberB} -> {power}");
+
+
+// START // ———————————————————————————————————————————————————————————————————————— 3 variant
+
+// int NumToPower(int numA, int numB)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= numB; i++)
+//     {
+//         result *= numA;
+//     }
+//     return result;
+// }
+
+// int InputNum(string num)
+// {
+//     Console.Write($"Input number {num}: ");
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int numberA = InputNum("A");
+// int numberB = InputNum("B");
+
+// Console.WriteLine($"{numberA} to the power of {numberB} -> {NumToPower(numberA, numberB)}");
+
+// ————————————————————————————————————————————————————————————————————————// END //
+
+
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+// START // ———————————————————————————————————————————————————————————————————————— 
+
+// int SumDigit(int num)
+// {
+//     int sum = 0;
+//     for (int i = 1; num > 1; i++)
+//     {
+//         sum = sum + num % 10;
+//         num = num / 10;
+//     }
+//     return sum;
+// }
+
+// Console.Write("Enter the number: ");
+// int number  = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(SumDigit(number));
+// ————————————————————————————————————————————————————————————————————————// END //
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+// START // ———————————————————————————————————————————————————————————————————————— 
+
+// int[] CreateArray(int size)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write($"Input a {i} element of array: ");
+//         array[i]= Convert.ToInt32(Console.ReadLine());
+//     }
+//     return array;
+// }
+
+// void ShowArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine($"Input count element of array: ");
+// int count = Convert.ToInt32(Console.ReadLine());
+// ShowArray(CreateArray(count));
+
+// ————————————————————————————————————————————————————————————————————————// END //
---
+++ C Seminar 05/Seminar 05 C-sharp/Program.cs
@@ -0,0 +1,78 @@
+﻿//Задача 1. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+// Найдите сумму отрицательных и положительных элементов массива.
+// int[] CreateRandomArray (int size, int minValue, int maxValue)
+// {
+//     int[] newArray = new int[size];
+//     for(int i = 0; i < size; i++)
+//     {
+//         newArray[i] = new Random().Next(minValue, maxValue+1);
+//     }
+//     return newArray;
+// }
+
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write (array[i] + " ");
+//     }
+//     Console.WriteLine ();
+// }
+
+// void FindSum (int[] array)
+// {
+//     int sumPos = 0;
+//     int sumNeg = 0;
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         if(array[i] >= 0) sumPos += array[i];
+//         else sumNeg += array[i];
+//     }
+
+//     Console.WriteLine ($"Sum of positive digits is -> {sumPos}, Sum of negative digits is -> {sumNeg}");
+// }
+
+
+// Console.WriteLine ("Input array size:");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Input minimal value of array element:");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Input maximal value of array element:");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateRandomArray (size, minValue, maxValue);
+// ShowArray (myArray);
+
+// FindSum (myArray);
+
+
+//Задача 2. Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+// int[] ChangeElementsOfArray (int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         array[i] *= (-1);
+//     }
+//     return array;
+// }
+
+// Console.WriteLine ("Input array size:");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Input minimal value of array element:");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Input maximal value of array element:");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateRandomArray (size, minValue, maxValue);
+// ShowArray(myArray);
+// myArray = ChangeElementsOfArray (myArray);
+// ShowArray(myArray);
+
+//Задача 3.Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+
+//Задача 4.****Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
+//значения которых лежат в отрезке [10,99].
+
